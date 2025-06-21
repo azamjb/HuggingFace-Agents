0fdd92866ea1fd2c1595bc6009bc1dd97d868fa2
azamjb <azamjb123@gmail.com>
2025-06-20 21:59:59 -0400

    codeAgent

---
+++ smolagentsCodeAgent.py
@@ -0,0 +1,56 @@
+from huggingface_hub import login
+import numpy as np
+import time
+import datetime
+
+login()
+
+from smolagents import CodeAgent, tool, DuckDuckGoSearchTool, LiteLLMModel
+
+m = LiteLLMModel(
+        model_id="ollama_chat/qwen2:7b",  
+        api_base="http://127.0.0.1:11434",  # Default Ollama local server
+        num_ctx=8192,
+    )
+
+# Tool to suggest a menu based on the occasion
+@tool
+def suggest_menu(occasion: str) -> str:
+    """
+    Suggests a menu based on the occasion.
+    Args:
+        occasion (str): The type of occasion for the party. Allowed values are:
+                        - "casual": Menu for casual party.
+                        - "formal": Menu for formal party.
+                        - "superhero": Menu for superhero party.
+                        - "custom": Custom menu.
+    """
+    if occasion == "casual":
+        return "Pizza, snacks, and drinks."
+    elif occasion == "formal":
+        return "3-course dinner with wine and dessert."
+    elif occasion == "superhero":
+        return "Buffet with high-energy and healthy food."
+    else:
+        return "Custom menu for the butler."
+    
+    
+#agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=m)
+#agent = CodeAgent(tools=[suggest_menu], model=m)
+agent = CodeAgent(tools=[], model=m, additional_authorized_imports=['datetime'])
+
+#agent.run("Search for the best music recommendations for a party at the Wayne's mansion.")
+
+#agent.run("Prepare a superhero menu for the pary.")
+
+agent.run(
+    """
+    Alfred needs to prepare for the party. Here are the tasks:
+    1. Prepare the drinks - 30 minutes
+    2. Decorate the mansion - 60 minutes
+    3. Set up the menu - 45 minutes
+    4. Prepare the music and playlist - 45 minutes
+
+    If we start right now, at what time will the party be ready?
+    """
+)
